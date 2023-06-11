2eef607b6be6475b3292f9bc8207f1216e887759
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-11 18:00:13 +0600

    create get selected class and payment api's

--- index.js
+++ index.js
@@ -246,6 +246,49 @@ app.delete("/selectClass/:id", async (req, res) => {
   res.send(result);
 });
 
+// get selected class
+app.get('/pay/:id',async(req,res)=>{
+  const id =req.params.id;
+  const query = {_id:new ObjectId(id)}
+  const result = await selectedClassCollection.find(query).toArray();
+  // console.log(result);
+  res.send(result)
+})
+
+
+// payment -----------
+
+   // create payment intent
+   app.post('/create-payment-intent', async (req, res) => {
+    const { price } = req.body;
+    const amount = parseInt(price * 100);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card']
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    })
+  })
+
+  
+    // payment related api
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      console.log(payment);
+      // const insertResult = await paymentCollection.insertOne(payment);
+
+      // const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+      // const deleteResult = await selectedClassCollection.deleteOne(query)
+
+      // res.send({ insertResult, deleteResult });
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
